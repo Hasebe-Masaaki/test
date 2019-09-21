a3a9db5da6964e0a2896b1fb6b10413852522652
22_Hasebe_Masaaki <hasebe516@kir.biglobe.ne.jp>
2019-09-22 02:35:36 +0900

    php chapter15 commit

--- techboost/mynews/app/Http/Controllers/Admin/ProfileController.php
+++ techboost/mynews/app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでNews Modelが扱えるようになる
+use App\Profile;
+use Auth;
+
 class ProfileController extends Controller
 {
     //関数を追加
@@ -12,8 +16,25 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-    public function create()
+    public function create(Request $request)
     {
+        // 以下を追記
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // ログインユーザのIDを取得
+        $form = $form + array('user_id' => Auth::id());
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
     }
 
--- techboost/mynews/resources/views/admin/profile/create.blade.php
+++ techboost/mynews/resources/views/admin/profile/create.blade.php
@@ -16,27 +16,32 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2" for="title">氏名(name)</label>
+                        <label class="col-md-2" for="name">氏名(name)</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2" for="title">性別(gender)</label>
+                        <label class="col-md-2" for="gender">性別(gender)</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            {{-- Form::select('gender', ['男性', '女性'], null, ['class' => 'form-control']) --}}
+                            <select class="form-control" name="gender" value="{{ old('gender') }}">
+                                <option value="">選択してください</option>
+                                <option value="male">男性</option>
+                                <option value="female">女性</option>
+                            </select>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2" for="title">趣味(hobby)</label>
+                        <label class="col-md-2" for="hobby">趣味(hobby)</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2" for="body">自己紹介欄(introduction)</label>
+                        <label class="col-md-2" for="introduction">自己紹介欄(introduction)</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
                     {{ csrf_field() }}
